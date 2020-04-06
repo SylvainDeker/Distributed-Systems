bf5bb7f8466c3b85f81c088bb83a2d49f910a496
Sylvain DEKER <sldkr@gmx.com>
2020-04-06 19:14:47 +0200

    Fix index issue

--- Tile/Tile.py
+++ Tile/Tile.py
@@ -4,37 +4,43 @@ import cv2 as cv
 from shapely.geometry import Polygon
 
 def zero_padding(dim,x0,y0,x1,y1):
-    # print("dim",dim)
-    # print("x0",x0)
-    # print("y0",y0)
-    # print("x1",x1)
-    # print("y1",y1)
+    edge_x,edge_y = min(dim[0]-x0,x1-x0), min(dim[1]-y0,y1-y0)
     lx,ly = np.clip(x1-dim[0],0,None), np.clip(y1-dim[1],0,None)
-    edge_x,edge_y = min(dim[0],x1), min(dim[1],y1)
-    # print("edge_x,edge_y",edge_x,edge_y)
-    # print("lx,ly",lx,ly)
+
     return (edge_x,edge_y,lx,ly)
 
 class Tile:
     """docstring for Tile."""
 
     def __init__(self,img,x0,y0,x1,y1):
+        dim = (img.shape[1],img.shape[2])
+        # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # print("dim",dim)
+        # print("x0",x0)
+        # print("y0",y0)
+        # print("x1",x1)
+        # print("y1",y1)
+
         assert x0>=0
         assert y0>=0
         assert x1>x0
         assert y1>y0
-        dim = (img.shape[1],img.shape[2])
         assert x0 < dim[0]
-        assert x1 < dim[1]
+        assert y0 < dim[1]
         self.points = ((x0,y0),(x1,y1))
 
         (edge_x,edge_y,lx,ly) = zero_padding(dim,x0,y0,x1,y1)
-        img_0_padded = np.pad(img[:,x0:edge_x,y0:edge_y], ((0,0),(0, lx),(0,ly)), 'constant', constant_values=0)
+
+        img_0_padded = np.pad(img[:,x0:edge_x+x0,y0:edge_y+y0], ((0,0),(0, lx),(0,ly)), 'constant', constant_values=0)
         self.img = np.copy(img_0_padded)
+
+        # print("x1-x0,y1-y0",x1-x0,y1-y0)
+        # print("lx,ly",lx,ly)
+        # print("edge_x,edge_y",edge_x,edge_y)
+
 
         assert x1-x0==self.img.shape[1]
         assert y1-y0==self.img.shape[2]
-
 
     def getPolygon(self):
         ((x0,y0),(x1,y1)) = self.points
@@ -46,8 +52,7 @@ class Tile:
         img = np.moveaxis(self.img,0,-1)
         img = cv.filter2D(img,-1,kernel)
         img = np.moveaxis(img,-1,0)
-        ((x0,y0),(x1,y1)) = self.points
-        return Tile(img,x0,y0,x1,y1)
+        self.img = img
 
 
 if __name__ == "__main__":
--- Tile/buildCollectionTile.py
+++ Tile/buildCollectionTile.py
@@ -3,8 +3,8 @@ import rasterio
 from Tile import Tile
 
 def buildCollectionTile(pathimage):
-    w=2
-    h=2
+    w=9
+    h=9
     data = rasterio.open(pathimage)
     img = np.array([data.read(1),data.read(2),data.read(3)])
     unit_height =int(data.height/h)
@@ -19,12 +19,42 @@ def buildCollectionTile(pathimage):
     start_w=0
     start_h=0
     collection = []
-    for i in range(itr_w):
-        for j in range(itr_h):
-            start_w = i * unit_width
+    print(itr_h,itr_w)
+    for j in range(itr_h):
+        for i in range(itr_w):
             start_h = j* unit_height
-            end_w = (i+1) * unit_width-1
-            end_h = (j+1) * unit_height-1
-            collection.append(Tile(img,start_w,start_h,end_w,end_h))
+            start_w = i * unit_width
+            end_h = (j+1) * unit_height
+            end_w = (i+1) * unit_width
+            collection.append(Tile(img,start_h,start_w,end_h,end_w))
 
     return (collection,data.height,data.width,len(data.indexes))
+
+
+if __name__ == '__main__':
+    import cv2 as cv
+
+    kernel = np.array([[0, -1, 0],
+                   [-1, 4, -1],
+                   [0, -1, 0]], np.float32)
+
+    (collection,height,width,chanels) = buildCollectionTile('../data/NE1_50M_SR_W/NE1_50M_SR_W.tif')
+
+    img = np.zeros((chanels,height,width))
+
+    for tile in collection:
+        ((x0,y0),(x1,y1)) = tile.points
+        tile.filter2D(kernel)
+        img[:,x0:x1,y0:y1] = tile.img
+    img = np.uint8(img)
+
+
+
+
+
+    tmp = np.moveaxis(img,0,-1)
+
+    print(tmp)
+    print(tmp.shape)
+
+    cv.imwrite("res.png",cv.cvtColor(tmp,cv.COLOR_RGB2BGR))
