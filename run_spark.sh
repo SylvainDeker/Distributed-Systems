0190c979c58cf5d38290f26eb7383c50e01d79be
Sylvain DEKER <sldkr@gmx.com>
2020-04-15 12:09:51 +0200

    SPARK: Fix troubles with imports

--- run_spark.sh
+++ run_spark.sh
@@ -1,8 +1,28 @@
 #! /bin/sh
 
 SPARK='./spark-3.0.0-preview2-bin-hadoop2.7/bin/spark-submit'
-PYTHONFILE=$1
-EXTRAFILE=$2
+MEM=4G
+MASTER=local[4]
+
+if [ $# -eq 0 ]
+then
+  MAIN=spark/try_spark.py
+  EXTRAFILES=Tile/Tile.py,Tile/buildCollectionTile.py
+fi
+if [ $# -eq 1 ]
+then
+  MAIN=$1
+  EXTRAFILES=Tile/Tile.py,Tile/buildCollectionTile.py
+fi
+if [ $# -eq 2 ]
+then
+  MAIN=$1
+  EXTRAFILES=$2
+fi
 
 
-PYSPARK_PYTHON=python3 ${SPARK} --master local[4] --py-files ${PYTHONFILE} ${EXTRAFILE}
+
+echo "run:\nPYSPARK_PYTHON=python3 ${SPARK} --master ${MASTER} --driver-memory ${MEM} --py-files ${EXTRAFILES} ${MAIN}\n"
+
+
+PYSPARK_PYTHON=python3 ${SPARK} --master ${MASTER} --driver-memory ${MEM} --py-files ${EXTRAFILES} ${MAIN}
--- spark/try_spark.py
+++ spark/try_spark.py
@@ -7,6 +7,7 @@ import numpy as np
 from pyspark import SparkContext
 from Tile.buildCollectionTile import build_collection_tile
 from Tile.Tile import Tile
+import rasterio
 
 
 
@@ -15,28 +16,29 @@ from Tile.Tile import Tile
                     './data/NE1_50M_SR_W/NE1_50M_SR_W.tif')
 
 sc = SparkContext()
-rdd1 = sc.parallelize(collection)
+rdd = sc.parallelize(collection)
 # TODO use rdd.toLocalIterator()
-kernel = np.array([[-1, -1, -1],
-                   [-1, 8, -1],
-                   [-1, -1, -1]], np.float32)
-rdd2 = rdd1.map(lambda n: n.filter2D(kernel))
-collection_res = rdd2.collect()
+kernel = np.array([[-2, -1, -2],
+                   [-1, 12, -1],
+                   [-2, -1, -2]], np.float32)
+rdd = rdd.map(lambda n: n.filter2D(kernel))
+collection_res = rdd.collect()
 
-# print(collection_res)
-# # build puzzle
-# img = np.zeros((channels, height, width))
-# for tile in collection_res:
-#     ((x0, y0), (x1, y1)) = tile.bounds
-#     img[:, x0:x1, y0:y1] = tile.img
-# res = rasterio.open("res.tiff", 'w',
-#                     driver=info.driver,
-#                     width=info.width,
-#                     height=info.height,
-#                     count=info.count,
-#                     dtype=info.dtypes[0],
-#                     crs=info.crs,
-#                     transform=info.transform)
-#
-# for i in range(res.count):
-#     res.write(img[i], i+1)
+print(len(collection_res))
+
+img = np.empty((info.count, info.height, info.width)).astype(info.dtypes[0])
+for tile in collection_res:
+    (x0, y0, x1, y1) = tile.bounding_polygon.bounds
+    (x0, y0, x1, y1) = (int(x0), int(y0), int(x1), int(y1))
+    img[:, x0:x1, y0:y1] = tile.img
+res = rasterio.open("res.tiff", 'w',
+                    driver=info.driver,
+                    width=info.width,
+                    height=info.height,
+                    count=info.count,
+                    dtype=info.dtypes[0],
+                    crs=info.crs,
+                    transform=info.transform)
+
+for i in range(res.count):
+    res.write(img[i], i+1)
