54eb33d3e156664e7cfc4eb2b1a7d5d6d3b5b60b
Sylvain DEKER <sldkr@gmx.com>
2020-05-06 18:19:43 +0200

    Remove Warning

--- test/test_buildCollectionTile.py
+++ test/test_buildCollectionTile.py
@@ -11,65 +11,65 @@ IMG_PATH_w9_h13 = 'data/NE1_50M_SR_W_w9_h13/NE1_50M_SR_W.tif'
 IMG_PATH_w12_h10 = 'data/NE1_50M_SR_W_w12_h10/NE1_50M_SR_W.tif'
 IMG_PATH_w13_h7 = 'data/NE1_50M_SR_W_w13_h7/NE1_50M_SR_W.tif'
 
-TestData = namedtuple(
-    "TestData",
+Data = namedtuple(
+    "Data",
     ('img_path', 'unit_height', 'unit_width', 'nb_expected_items')
 )
 
 @pytest.mark.parametrize(
     "data",
     (
-            TestData(img_path=IMG_PATH,
+            Data(img_path=IMG_PATH,
                      unit_height=500,
                      unit_width=500,
                      nb_expected_items=242),
 
-            TestData(img_path=IMG_PATH,
+            Data(img_path=IMG_PATH,
                      unit_height=1000,
                      unit_width=1000,
                      nb_expected_items=66),
 
-            TestData(img_path=IMG_PATH_w9_h13,
+            Data(img_path=IMG_PATH_w9_h13,
                      unit_height=3,
                      unit_width=3,
                      nb_expected_items=15),
 
-            TestData(img_path=IMG_PATH_w9_h13,
+            Data(img_path=IMG_PATH_w9_h13,
                      unit_height=6,
                      unit_width=4,
                      nb_expected_items=9),
 
-            TestData(img_path=IMG_PATH_w9_h13,
+            Data(img_path=IMG_PATH_w9_h13,
                      unit_height=13,
                      unit_width=9,
                      nb_expected_items=1),
 
-            TestData(img_path=IMG_PATH_w12_h10,
+            Data(img_path=IMG_PATH_w12_h10,
                      unit_height=3,
                      unit_width=3,
                      nb_expected_items=16),
 
-            TestData(img_path=IMG_PATH_w12_h10,
+            Data(img_path=IMG_PATH_w12_h10,
                      unit_height=6,
                      unit_width=4,
                      nb_expected_items=6),
 
-            TestData(img_path=IMG_PATH_w12_h10,
+            Data(img_path=IMG_PATH_w12_h10,
                      unit_height=10,
                      unit_width=12,
                      nb_expected_items=1),
 
-            TestData(img_path=IMG_PATH_w13_h7,
+            Data(img_path=IMG_PATH_w13_h7,
                      unit_height=3,
                      unit_width=3,
                      nb_expected_items=15),
 
-            TestData(img_path=IMG_PATH_w13_h7,
+            Data(img_path=IMG_PATH_w13_h7,
                      unit_height=6,
                      unit_width=4,
                      nb_expected_items=8),
 
-            TestData(img_path=IMG_PATH_w13_h7,
+            Data(img_path=IMG_PATH_w13_h7,
                      unit_height=7,
                      unit_width=13,
                      nb_expected_items=1)
