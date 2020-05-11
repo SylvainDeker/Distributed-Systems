cca16c6ea40b811d0a6b43f18b71ab6aa2d7a904
Sylvain DEKER <sldkr@gmx.com>
2020-05-11 19:30:22 +0200

    New dark.py and new test_dark.py

--- distributed_systems/dark.py
+++ distributed_systems/dark.py
@@ -9,227 +9,81 @@ import dask
 import yaml
 
 
-def load_config(pathfile):
-    # Default value
-    data = {'imgleft': { 'gain': 1,
-                         'mean': 0,
-                         'stddev': 1},
-            'imgright':{ 'gain': 1,
-                         'mean': 0,
-                         'stddev': 1}
-            }
+class Dark:
+    def __init__(self):
+        self.shape_image = (0, 0)
+        self.shape_tile = (500, 500)
 
-    with open(pathfile,"r") as src:
-        data = yaml.safe_load(src)
+    # def __init__(self, shape_image, shape_tile = (500,500)):
+    #     (height, width) = shape_image
+    #     self.shape_image = (height, width)
+    #     self.shape_tile = shape_tile
 
-    return data
+    @property
+    def shape_all_tiles(self):
+        nb_tile_h = int(self.shape_image[0] / self.shape_tile[0]) + (self.shape_image[0] % self.shape_tile[0] > 0)
+        nb_tile_w = int(self.shape_image[1] / self.shape_tile[1]) + (self.shape_image[1] % self.shape_tile[1] > 0)
+        return (nb_tile_h, nb_tile_w)
 
-def extract_collections(pathimage,unit_height=500,unit_width=500):
+    @property
+    def vertical_frontiers(self):
+        (_, nb_tile_w) = self.shape_all_tiles
+        return (int(nb_tile_w/3),int((2*nb_tile_w)/3))
 
-    with rasterio.open(pathimage) as data:
-        info = data
+    @property
+    def shape_collection_left(self):
+        (nb_tile_h, nb_tile_w) = self.shape_all_tiles
+        (vf1, vf2) = self.vertical_frontiers
+        return (nb_tile_h, vf2 )
 
-    itr_h = int(info.height/unit_height) + (info.height % unit_height > 0)
-    itr_w = int(info.width/unit_width) + (info.width % unit_width > 0)
-
-    tile_lim_1 = int((itr_w)/3)
-    tile_lim_2 = int((itr_w*2)/3)
-
-    px_lim_1 = tile_lim_1 * unit_width
-    px_lim_2 = tile_lim_2 * unit_width
-
-
-    collection1 = []
-    collection2 = []
-    nb_item_c1 = 0
-    nb_item_c2 = 0
-    list_intersection_c1_c2 = []
-    for i, j in itertools.product(range(itr_h), range(itr_w)):
-        x0 = i * unit_height
-        y0 = j * unit_width
-        x1 = (i+1) * unit_height
-        y1 = (j+1) * unit_width
-
-        t1 = None
-        t2 = None
-        if y0 < px_lim_2:
-            nb_item_c1 += 1
-            t1 = Tile(pathimage,
-                     Polygon([(x0, y0),
-                              (x1, y0),
-                              (x1, y1),
-                              (x0, y1)]),
-                     (i,j))
-            collection1.append(t1)
-
-        if y0 >= px_lim_1:
-            nb_item_c2 += 1
-            t2 = Tile(pathimage,
-                      Polygon([(x0, y0),
-                               (x1, y0),
-                               (x1, y1),
-                               (x0, y1)]),
-                        (i,j))
-            collection2.append(t2)
-
-        if y0 >= px_lim_1 and y0 < px_lim_2 and t1 != None and t2 != None:
-            list_intersection_c1_c2.append((len(collection1)-1,len(collection2)))
-
-    nb_unit_width_collection1 = tile_lim_2
-    nb_unit_width_collection2 = itr_w - tile_lim_1
-
-    return (collection1,
-            collection2,
-            info,
-            nb_unit_width_collection1,
-            nb_unit_width_collection2,
-            nb_item_c1,
-            nb_item_c2,
-            nb_unit_width_collection1 * unit_width,
-            nb_unit_width_collection2 * unit_width -
-                (unit_width - (info.width % unit_width)) *
-                (info.width%unit_width > 0),
-            list_intersection_c1_c2 )
+    @property
+    def shape_collection_right(self):
+        (nb_tile_h, nb_tile_w) = self.shape_all_tiles
+        (vf1, vf2) = self.vertical_frontiers
+        return (nb_tile_h, nb_tile_w - vf1 )
 
 
-def run_dask(pathimage, pathconfig):
-    client = Client()
-    # DARK-FDR-001 step 1
-    (collection1, collection2, info, _, _, _, _, w1, w2,list_intersection_c1_c2) = extract_collections(
-                                    pathimage)
-    config = load_config(pathconfig)
+    @property
+    def shape_collection_intersection(self):
+        (nb_tile_h, nb_tile_w) = self.shape_all_tiles
+        (vf1, vf2) = self.vertical_frontiers
+        return (nb_tile_h, vf2 - vf1 )
 
+    @property
+    def shape_image_left(self):
+        (sih, siw) = self.shape_image
+        (sclh, sclw) = self.shape_collection_left
+        (sth, stw) = self.shape_tile
+        return (sih, sclw*stw)
 
-    # DARK-FDR-001 step 2
-    collection1_res = []
-    collection2_res = []
-    for t in collection1:
-        (x0, y0, x1, y1) = t.bounding_polygon.bounds
-        (x0, y0, x1, y1) = (int(x0), int(y0), int(x1), int(y1))
-        e = dask.delayed(t.add_noise)(config['imgleft']['gain'],
-                                      config['imgleft']['mean'],
-                                      config['imgleft']['stddev'])
-        collection1_res.append(e.compute())
+    @property
+    def shape_image_right(self):
+        (sih, siw) = self.shape_image
+        (scrh, scrw) = self.shape_collection_right
+        (sth, stw) = self.shape_tile
+        h = sih
+        w = scrw * stw - (stw - (siw % stw)) * (siw % stw > 0)
+        return (h, w)
 
-    for t in collection2:
-        (x0, y0, x1, y1) = t.bounding_polygon.bounds
-        (x0, y0, x1, y1) = (int(x0), int(y0), int(x1), int(y1))
-        e = dask.delayed(t.add_noise)(config['imgright']['gain'],
-                                      config['imgright']['mean'],
-                                      config['imgright']['stddev'])
-        collection2_res.append(e.compute())
+    def coord_collection_from_right_to_left(self, i, j):
+        return (i, j + self.vertical_frontiers[0] )
 
-    collection1 = collection1_res
-    collection2 = collection2_res
+    def coord_collection_from_left_to_right(self, i, j):
+        return (i, j - self.vertical_frontiers[0])
 
-    # DARK-FDR-001 step 3
-    # Already given by list_intersection_c1_c2
+    def coord_collection_from_right_to_global(self, i, j):
+        return (i, j + self.vertical_frontiers[0])
 
-    # DARK-FDR-001 step 4 & 5
-    computed_gains1 = np.empty((len(list_intersection_c1_c2),len(info.indexes)))
-    computed_gains2 = np.empty((len(list_intersection_c1_c2),len(info.indexes)))
-    for i in range(len(list_intersection_c1_c2)):
-        mr1 = collection1[list_intersection_c1_c2[i][0]].mean_radiosity
-        mr2 = collection2[list_intersection_c1_c2[i][1]].mean_radiosity
-        computed_gains1[i] = np.sqrt(mr2/mr1)
-        computed_gains2[i] = np.sqrt(mr1/mr2)
+    def coord_collection_from_left_to_global(self, i, j):
+        return (i, j)
 
-    # DARK-FDR-001 step 6
-    computed_gains1 = np.moveaxis(computed_gains1,1,0)
-    computed_gains2 = np.moveaxis(computed_gains2,1,0)
-    for i in range(len(info.indexes)):
-        computed_gains1[i] = np.convolve(computed_gains1[i],
-                                         config['kernel_gain'],
-                                         'same')
-        computed_gains2[i] = np.convolve(computed_gains2[i],
-                                         config['kernel_gain'],
-                                         'same')
-    computed_gains1 = np.moveaxis(computed_gains1,1,0)
-    computed_gains2 = np.moveaxis(computed_gains2,1,0)
+    def list_intersection_coord_glob(self):
+        (nb_tile_h, nb_tile_w) = self.shape_all_tiles
+        (vf1, vf2) = self.vertical_frontiers
 
-    # DARK-FDR-001 step 7
-    intersection_res = []
-    for i in range(len(list_intersection_c1_c2)):
-        e1 = dask.delayed(collection1[list_intersection_c1_c2[i][0]].apply_gain)(computed_gains1[i])
-        e2 = dask.delayed(collection2[list_intersection_c1_c2[i][1]].apply_gain)(computed_gains2[i])
-        intersection_res.append((e1.compute(), e2.comptue()))
-
-    # DARK-FDR-001 step 8
-    res4 = []
-    for i in range(len(intersection_res)):
-        e1 = dask.delayed(intersection_res[i][0].filter2D)(np.array(config['kernel_blur']))
-        e2 = dask.delayed(intersection_res[i][1].filter2D)(np.array(config['kernel_blur']))
-        res4.append((e1.compute(), e2.comptue()))
-
-
-    with rasterio.open('collection1.tif', 'w',
-                       driver=info.driver,
-                       width=w1, height=info.height, count=info.count,
-                       dtype=info.dtypes[0], transform=info.transform) as dst:
-
-        for t in collection1:
-            (x0, y0, x1, y1) = t.bounding_polygon.bounds
-            (x0, y0, x1, y1) = (int(x0), int(y0), int(x1), int(y1))
-            for i in info.indexes:
-                dst.write(t.img[i-1],
-                          window=Window(y0, x0, y1-y0, x1-x0),
-                          indexes=i)
-
-    dec = info.width - w2
-    with rasterio.open('collection2.tif', 'w',
-                       driver=info.driver,
-                       width=w2, height=info.height, count=info.count,
-                       dtype=info.dtypes[0], transform=info.transform) as dst:
-
-        for t in collection2:
-            (x0, y0, x1, y1) = t.bounding_polygon.bounds
-            (x0, y0, x1, y1) = (int(x0), int(y0-dec), int(x1), int(y1-dec))
-            for i in info.indexes:
-                dst.write(t.img[i-1],
-                          window=Window(y0, x0, y1-y0, x1-x0),
-                          indexes=i)
-
-    with rasterio.open('All.tif', 'w',
-                       driver=info.driver,
-                       width=info.width, height=info.height, count=info.count,
-                       dtype=info.dtypes[0], transform=info.transform) as dst:
-        for t in collection1:
-            (x0, y0, x1, y1) = t.bounding_polygon.bounds
-            (x0, y0, x1, y1) = (int(x0), int(y0), int(x1), int(y1))
-            for i in info.indexes:
-                dst.write(t.img[i-1],
-                          window=Window(y0, x0, y1-y0, x1-x0),
-                          indexes=i)
-
-        for t in collection2:
-            (x0, y0, x1, y1) = t.bounding_polygon.bounds
-            (x0, y0, x1, y1) = (int(x0), int(y0), int(x1), int(y1))
-            for i in info.indexes:
-                dst.write(t.img[i-1],
-                          window=Window(y0, x0, y1-y0, x1-x0),
-                          indexes=i)
-        for (t,_) in res4:
-            (x0, y0, x1, y1) = t.bounding_polygon.bounds
-            (x0, y0, x1, y1) = (int(x0), int(y0), int(x1), int(y1))
-            for i in info.indexes:
-                dst.write(t.img[i-1],
-                          window=Window(y0, x0, y1-y0, x1-x0),
-                          indexes=i)
-
-
-    client.close()
-
-
-
-
-
-if __name__ == '__main__':
-    from collections import OrderedDict
-    from shapely.geometry import mapping
-    import fiona
-    import pprint
-
-    # r = load_config('config.yaml')
-    # print(r)
-
-    run_dask('data/NE1_50M_SR_W/NE1_50M_SR_W.tif', 'config.yaml')
+        list = []
+        for i, j in itertools.product(range(nb_tile_h), range(nb_tile_w)):
+            y0 = j * self.shape_tile[1]
+            if j >= vf1 and j < vf2:
+                list.append((i, j))
+        return list
--- test/test_dark.py
+++ test/test_dark.py
@@ -4,7 +4,7 @@ import pytest
 
 from collections import namedtuple
 
-from distributed_systems.dark import extract_collections
+from distributed_systems.dark import Dark
 
 IMG_PATH = 'data/NE1_50M_SR_W/NE1_50M_SR_W.tif'
 IMG_PATH_w9_h13 = 'data/NE1_50M_SR_W_w9_h13/NE1_50M_SR_W.tif'
@@ -14,171 +14,88 @@ IMG_PATH_w13_h7 = 'data/NE1_50M_SR_W_w13_h7/NE1_50M_SR_W.tif'
 
 Data = namedtuple(
     "Data",
-    ('img_path',
-     'unit_height',
-     'unit_width',
-     'expected_nb_unit_width_collection1',
-     'expected_nb_unit_width_collection2',
-     'expected_nb_item_collection1',
-     'expected_nb_item_collection2',
-     'expected_width_img1',
-     'expected_width_img2',
+    (    'shape_image',
+         'shape_tile',
+         'expected_shape_all_tiles',
+         'expected_vertical_frontiers',
+         'expected_shape_collection_left',
+         'expected_shape_collection_right',
+         'expected_shape_image_left',
+         'expected_shape_image_right',
+         'expected_list_intersection_coord_glob'
      )
 )
 
 @pytest.mark.parametrize(
     "data",
     (
-        # Data(img_path=IMG_PATH,
-        #          unit_height=500,
-        #          unit_width=500,
-        #          expected_nb_unit_width_collection1 = 2,
-        #          expected_nb_unit_width_collection2 = 2,
-        #          expected_nb_item_collection1 = 10,
-        #          expected_nb_item_collection2 = 10,
-        #          expected_width_img1 = 6,
-        #          expected_width_img2 = 6
-        #          ),
-        #
-        # Data(img_path=IMG_PATH,
-        #          unit_height=1000,
-        #          unit_width=1000,
-        #          expected_nb_unit_width_collection1 = 2,
-        #          expected_nb_unit_width_collection2 = 2,
-        #          expected_nb_item_collection1 = 10,
-        #          expected_nb_item_collection2 = 10,
-        #          expected_width_img1 = 6,
-        #          expected_width_img2 = 6
-        #          ),
+        Data(shape_image = (8,10),
+             shape_tile = (7,3),
+             expected_shape_all_tiles = (2,4),
+             expected_vertical_frontiers = (1,2),
+             expected_shape_collection_left = (2,2),
+             expected_shape_collection_right = (2,3),
+             expected_shape_image_left = (8,6),
+             expected_shape_image_right = (8,7),
+             expected_list_intersection_coord_glob = [(0,1),(1,1)]
+             ),
 
-        Data(img_path=IMG_PATH_w9_h13,
-                 unit_height=3,
-                 unit_width=3,
-                 expected_nb_unit_width_collection1 = 2,
-                 expected_nb_unit_width_collection2 = 2,
-                 expected_nb_item_collection1 = 10,
-                 expected_nb_item_collection2 = 10,
-                 expected_width_img1 = 6,
-                 expected_width_img2 = 6
-                 ),
+        Data(shape_image = (8,10),
+             shape_tile = (2,4),
+             expected_shape_all_tiles = (4,3),
+             expected_vertical_frontiers = (1,2),
+             expected_shape_collection_left = (4,2),
+             expected_shape_collection_right = (4,2),
+             expected_shape_image_left = (8,8),
+             expected_shape_image_right = (8,6),
+             expected_list_intersection_coord_glob = [(0,1),(1,1),(2,1),(3,1)]
+             ),
 
-        Data(img_path=IMG_PATH_w9_h13,
-                 unit_height=6,
-                 unit_width=4,
-                 expected_nb_unit_width_collection1 = 2,
-                 expected_nb_unit_width_collection2 = 2,
-                 expected_nb_item_collection1 = 6,
-                 expected_nb_item_collection2 = 6,
-                 expected_width_img1 = 8,
-                 expected_width_img2 = 5
-                 ),
+        Data(shape_image = (11,13),
+             shape_tile = (7,3),
+             expected_shape_all_tiles = (2,5),
+             expected_vertical_frontiers = (1,3),
+             expected_shape_collection_left = (2,3),
+             expected_shape_collection_right = (2,4),
+             expected_shape_image_left = (11,9),
+             expected_shape_image_right = (11,10),
+             expected_list_intersection_coord_glob = [(0,1),(1,1),
+                                                      (0,2),(1,2)]
+             ),
 
-        Data(img_path=IMG_PATH_w9_h13,
-                 unit_height=13,
-                 unit_width=9,
-                 expected_nb_unit_width_collection1 = 0,
-                 expected_nb_unit_width_collection2 = 1,
-                 expected_nb_item_collection1 = 0,
-                 expected_nb_item_collection2 = 1,
-                 expected_width_img1 = 0,
-                 expected_width_img2 = 9
-                 ),
-
-        Data(img_path=IMG_PATH_w12_h10,
-                 unit_height=3,
-                 unit_width=3,
-                 expected_nb_unit_width_collection1 = 2,
-                 expected_nb_unit_width_collection2 = 3,
-                 expected_nb_item_collection1 = 8,
-                 expected_nb_item_collection2 = 12,
-                 expected_width_img1 = 6,
-                 expected_width_img2 = 9
-                 ),
-
-        Data(img_path=IMG_PATH_w12_h10,
-                 unit_height=6,
-                 unit_width=4,
-                 expected_nb_unit_width_collection1 = 2,
-                 expected_nb_unit_width_collection2 = 2,
-                 expected_nb_item_collection1 = 4,
-                 expected_nb_item_collection2 = 4,
-                 expected_width_img1 = 8,
-                 expected_width_img2 = 8
-                 ),
-
-        Data(img_path=IMG_PATH_w12_h10,
-                 unit_height=10,
-                 unit_width=12,
-                 expected_nb_unit_width_collection1 = 0,
-                 expected_nb_unit_width_collection2 = 1,
-                 expected_nb_item_collection1 = 0,
-                 expected_nb_item_collection2 = 1,
-                 expected_width_img1 = 0,
-                 expected_width_img2 = 12
-                 ),
-
-        Data(img_path=IMG_PATH_w13_h7,
-                 unit_height=3,
-                 unit_width=3,
-                 expected_nb_unit_width_collection1 = 3,
-                 expected_nb_unit_width_collection2 = 4,
-                 expected_nb_item_collection1 = 9,
-                 expected_nb_item_collection2 = 12,
-                 expected_width_img1 = 9,
-                 expected_width_img2 = 10
-                 ),
-
-        Data(img_path=IMG_PATH_w13_h7,
-                 unit_height=6,
-                 unit_width=4,
-                 expected_nb_unit_width_collection1 = 2,
-                 expected_nb_unit_width_collection2 = 3,
-                 expected_nb_item_collection1 = 4,
-                 expected_nb_item_collection2 = 6,
-                 expected_width_img1 = 8,
-                 expected_width_img2 = 9
-                 ),
-
-        Data(img_path=IMG_PATH_w13_h7,
-                 unit_height=7,
-                 unit_width=13,
-                 expected_nb_unit_width_collection1 = 0,
-                 expected_nb_unit_width_collection2 = 1,
-                 expected_nb_item_collection1 = 0,
-                 expected_nb_item_collection2 = 1,
-                 expected_width_img1 = 0,
-                 expected_width_img2 = 13
-                 ),
+        Data(shape_image = (11,13),
+             shape_tile = (2,4),
+             expected_shape_all_tiles = (6,4),
+             expected_vertical_frontiers = (1,2),
+             expected_shape_collection_left = (6,2),
+             expected_shape_collection_right = (6,3),
+             expected_shape_image_left = (11,8),
+             expected_shape_image_right = (11,9),
+             expected_list_intersection_coord_glob = [(0,1),
+                                                      (1,1),
+                                                      (2,1),
+                                                      (3,1),
+                                                      (4,1),
+                                                      (5,1)]
+             )
 
     ),
     ids=(
-        "w9_h13_3_3",
-        "w9_h13_6_4",
-        "w9_h13_u",
-        "w12_h10_3_3",
-        "w12_h10_6_4",
-        "w12_h10_u",
-        "w13_h7_3_3",
-        "w13_h7_6_4",
-        "w13_h7_u",
+        "ih8_iw10_th7_tw3",
+        "ih8_iw10_th2_tw4",
+        "ih11_iw13_th7_tw3",
+        "ih11_iw13_th2_tw4",
     )
 )
-def test_collection(data):
-    (collection1,
-     collection2,
-     info,
-     nb_unit_width_collection1,
-     nb_unit_width_collection2,
-     nb_item_c1,
-     nb_item_c1,
-     width_img1,
-     width_img2, _ ) = extract_collections(data.img_path,
-                                        data.unit_height,
-                                        data.unit_width)
+def test_indices(data):
+    dark = Dark()
+    dark.shape_image = data.shape_image
+    dark.shape_tile = data.shape_tile
 
-    assert len(collection1) == data.expected_nb_item_collection1
-    assert len(collection2) == data.expected_nb_item_collection2
-    assert nb_unit_width_collection1 == data.expected_nb_unit_width_collection1
-    assert nb_unit_width_collection2 == data.expected_nb_unit_width_collection2
-    assert width_img1 == data.expected_width_img1
-    assert width_img2 == data.expected_width_img2
+    assert data.expected_shape_all_tiles == dark.shape_all_tiles
+    assert data.expected_vertical_frontiers == dark.vertical_frontiers
+    assert data.expected_shape_collection_left == dark.shape_collection_left
+    assert data.expected_shape_collection_right == dark.shape_collection_right
+    assert data.expected_shape_image_left == dark.shape_image_left
+    assert data.expected_shape_image_right == dark.shape_image_right
+    assert set(data.expected_list_intersection_coord_glob) == set(dark.list_intersection_coord_glob())
