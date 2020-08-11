0d7ee6baa7d104743db1a1464aee437d23fefb9f
Sylvain DEKER <sldkr@gmx.com>
2020-08-11 23:30:33 +0200

    Netdata

--- distributed_systems/flatMapreducebyKeyProfi.py
+++ distributed_systems/flatMapreducebyKeyProfi.py
@@ -58,4 +58,4 @@ if __name__ == '__main__':
 
 
 
-    input("wait")
+    # input("wait")
--- distributed_systems/netdata.py
+++ distributed_systems/netdata.py
@@ -3,28 +3,48 @@ import numpy as np
 import time
 import matplotlib
 import matplotlib.pyplot as plt
-if __name__ == '__main__':
-    start_time = int(time.time())
-    print(start_time)
-    time.sleep(10)
-    end_time = int(time.time())
-    print(end_time)
-    r = requests.get("http://192.168.0.16:19999/api/v1/data?chart=system.cpu&after="+str(start_time)+"&before="+str(end_time)+"&points=5000&group=average&format=json&options=seconds&options=jsonwrap")
 
+def get_data(start_time,end_time,chart,idx):
+    url = "http://localhost:19999/api/v1/data?chart="+chart+"&after="+str(start_time)+"&before="+str(end_time)+"&points=5000&group=average&format=json&options=seconds&options=jsonwrap"
+    print(url)
+    r = requests.get(url)
     if r.status_code>=200 and r.status_code<300:
         data = r.json()['result']['data']
         data = np.array(data)
-        print(data)
-        # Data for plotting
-        t = np.arange(0.0,len(data),1)
-        s = data[:,6:7]
+        return np.flip(data[:,idx:idx+1])
+    return None
 
-        fig, ax = plt.subplots()
-        ax.plot(t, s)
 
-        ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-               title='About as simple as it gets, folks')
-        ax.grid()
+if __name__ == '__main__':
+    start_time = int(time.time())
+    print(start_time)
+    time.sleep(60)
+    end_time = int(time.time())
+    print(end_time)
 
-        fig.savefig("test.png")
-        # plt.show()
+    cpu0 = get_data(start_time,end_time,'cpu.cpu0',6)
+    cpu1 = get_data(start_time,end_time,'cpu.cpu1',6)
+    cpu2 = get_data(start_time,end_time,'cpu.cpu2',6)
+    cpu3 = get_data(start_time,end_time,'cpu.cpu3',6)
+    mem = get_data(start_time,end_time,'system.ram',2)
+
+    t = np.arange(0,len(cpu0),1)
+
+    fig, ax = plt.subplots(2,1)
+    ax[0].plot(t, cpu0)
+    ax[0].plot(t, cpu1)
+    ax[0].plot(t, cpu2)
+    ax[0].plot(t, cpu3)
+    ax[1].plot(t, mem)
+
+    ax[0].set(xlabel='Temps (Seconde)',
+           ylabel='Usage %',
+           title='Utilisation CPU')
+    ax[0].set_ylim(bottom=0,top=100)
+    ax[1].set_ylim(bottom=0,top=9000)
+
+    ax[0].grid()
+    ax[1].grid()
+
+    # fig.savefig("test.png")
+    plt.show()
